576cb609739fb672d9bc5e60fe00171cadcc701c
Preston Seay <preston.seay@outlook.com>
2025-08-20 14:24:19 -0700

    renamed functions to match native JS/Py

--- src/plugins/console/languages/BasicJS/implementation.ts
+++ src/plugins/console/languages/BasicJS/implementation.ts
@@ -4,19 +4,38 @@ const createExports = (
   sendMessage: (message: FromRuntimeMessage) => void,
   onMessage: Function
 ) => {
-  return Promise.resolve({
-    writeOutput: (output: string) => {
-      sendMessage({ output });
-    },
-    getInput: async (inputMessage: string) => {
-      const promise = new Promise<string>((res) => {
-        const unsubscribe = onMessage((message: any) => {
-          res(message.input);
-          unsubscribe();
-        });
+  // define funcionts for I/O messaging
+  function sendOutput(output: string) {
+    sendMessage({ output });
+  }
+  function getInput(input: string) {
+    const promise = new Promise<string>((res) => {
+      const unsubscribe = onMessage((message: any) => {
+        res(message.input);
+        unsubscribe();
       });
-      sendMessage({ input: inputMessage });
-      return promise;
+    });
+    sendMessage({ input });
+    return promise;
+  }
+
+  // Return user functions.
+  return Promise.resolve({
+    // Python version
+    print: (output: string) => {
+      sendOutput(output);
+    },
+    input: (inputMessage: string) => {
+      return getInput(inputMessage);
+    },
+    // JavaScript version
+    console: {
+      log: (output: string) => {
+        sendOutput(output);
+      },
+    },
+    prompt: (inputMessage: string) => {
+      return getInput(inputMessage);
     },
   });
 };
