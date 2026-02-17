02353b12184be1ef6582dc25fc80d432db8006fe
katecode314 <116534751+katecode314@users.noreply.github.com>
2026-02-17 15:45:11 -0800

    minor

--- src/plugins/haber-reaction/languages/BasicJS/starter-code/main.js
+++ src/plugins/haber-reaction/languages/BasicJS/starter-code/main.js
@@ -1,16 +1,15 @@
 function resulting_nh3() {
 
-    let h2 = 0;
-    let n2 = 0;
+    let h2 = 5;
+    let n2 = 3;
+    // TODO 3: define needed_n2 here
+
+    // TODO 1: define nh3_made variables here
 
     let nh3_made = 0;
-    let limiting = "???"; // write "n2" or "h2" or "None"
+    let limiting = ""; // write "n2" or "h2" or "None"
 
-    // How much NH3 could we make if ONLY N2 limited us?
-    // const nh3_from_n2 = n2 * 2;
-
-    // How much NH3 could we make if ONLY H2 limited us?
-    // const nh3_from_h2 = h2 * (2/3);
+    // TODO 2: write if statements here
 
     return [h2, n2, nh3_made, limiting];
 }
